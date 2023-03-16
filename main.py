f0ebfaa7ff7ebaee972ff1f4f827c0101e6f620c
faisal-fida <fa20-bcs-004@cuiwah.edu.pk>
2023-03-16 19:49:53 +0500

    added required changes

--- main.py
+++ main.py
@@ -9,115 +9,66 @@ from data_processing.initial import data_headers_payloads
 
 URL = "https://www.thredup.com/api/v1/shop/graphql"
 
-payload_initial_products = data_headers_payloads[0]
-headers_initial_products = data_headers_payloads[1]
-payload_final_products = data_headers_payloads[2]
-headers_final_products = data_headers_payloads[3]
+payload_products = data_headers_payloads[0]
+headers_products = data_headers_payloads[1]
+
+with open('data_processing/itemnumbers.txt', 'r') as re:
+    itemNumbersList = re.read().splitlines()
 
 
 def post_request(URL, payload, headers):
     return requests.request("POST", URL, json=payload, headers=headers).json()
 
 
-def json_variables(itemNumber=137186287, json_amount=119, type_products='final', json_page=2, json_visitorId=33, json_parentId=74, json_queryId=746):
-    if type_products == 'initial':
-        payload_initial_products['variables']['amount'] = json_amount
-        payload_initial_products['variables']['page'] = json_page
-        # payload_initial_products['variables']['visitorId'] = json_visitorId
-        # payload_initial_products['variables']['bucketingKey'] = json_visitorId
-        # payload_initial_products['variables']['parentId'] = json_parentId
-        # payload_initial_products['variables']['queryId'] = json_queryId
-        # headers_initial_products['auth-token'] = auth_token
-        # headers_initial_products['cookie'] = cookie
-    else:
-        payload_final_products['variables']['itemNumber'] = itemNumber
-        # payload_final_products['variables']['visitorId'] = json_visitorId
-        # payload_final_products['variables']['queryId'] = json_queryId
-        # payload_final_products['variables']['resultId'] = json_resultId
-        # headers_final_products['auth-token'] = auth_token
-        # headers_final_products['cookie'] = cookie
+def json_variables(itemNumber=137186287):
+    payload_products['variables']['itemNumber'] = itemNumber
+    # payload_products['variables']['visitorId'] = json_visitorId
+    # payload_products['variables']['queryId'] = json_queryId
+    # payload_products['variables']['resultId'] = json_resultId
+    # headers_products['auth-token'] = auth_token
+    # headers_products['cookie'] = cookie
 
 
-def get_initial_products():
-    data_initial_products = []
-    # start_time = time.time()
-
-    for idx in range(1, 2):
-        print(f'Scraping Initial Products Page: {idx}', end='\r')
-        json_variables(type_products='initial', json_page=idx)
-        try:
-            data_initial_product = post_request(
-                URL, payload_initial_products, headers_initial_products)['data']['items']['nodes']
-            data_initial_products.append(data_initial_product)
-        except Exception as e:
-            print(f"\nError retrieving data_initial_product: {e} \n")
-            pass
-        time.sleep(20)
-
-    # end_time = time.time()
-    # print(f"Time taken to Scrape {idx} Pages: {end_time - start_time}")
-
-    return data_initial_products
-
-
-def get_final_products(itemNumbersList):
-    data_final_products = []
+def get_products(itemNumbersList):
+    data_products = []
     start_time = time.time()
 
     for idx, itemNumber in enumerate(itemNumbersList[:], 1):
         print(f'Scraping Final Product No: {idx} {itemNumber}', end='\r')
         json_variables(itemNumber)
         try:
-            data_final_product = post_request(
-                URL, payload_final_products, headers_final_products)['data']
-            data_final_products.append(data_final_product)
+            data_product = post_request(URL, payload_products, headers_products)['data']
+            data_products.append(data_product)
             itemNumbersList.remove(itemNumber)
-            if idx == 10:
-                break
+            # if idx == 10:
+            #     break
         except Exception as e:
-            print(f"\nError retrieving data_final_product: {e} \n")
+            print(f"\nError retrieving data_product: {e} \n")
             pass
         time.sleep(random.uniform(1, 2))
 
     end_time = time.time()
     print(f"Time taken to Scrape {idx} Products: {end_time - start_time}")
 
-    return data_final_products
+    return data_products
 
 
-def parse_products(data_products, type_products='final'):
+def parse_products(data_products):
     df_products = pd.DataFrame()
 
-    if type_products == 'final':
-        for data_product in data_products:
-            df_product = pd.DataFrame.from_dict(
-                data_product['itemByItemNumber'], orient='index')
-            df_product = df_product.transpose()
-            df_products = pd.concat(
-                [df_products, df_product], ignore_index=True)
-        return df_products
-
-    else:
-        for data_product in data_products:
-            df_product = pd.DataFrame(data_product)
-            df_products = pd.concat(
-                [df_products, df_product], ignore_index=True)
-        return df_products['itemNumber'].to_list()
+    for data_product in data_products:
+        df_product = pd.DataFrame.from_dict(data_product['itemByItemNumber'], orient='index')
+        df_product = df_product.transpose()
+        df_products = pd.concat([df_products, df_product], ignore_index=True)
+        df_products.to_csv('output/df_products.csv')
 
 
 def main():
-    data_initial_products = get_initial_products()
-    itemNumbersList = parse_products(
-        data_initial_products, type_products='initial')
-
-    data_final_products = get_final_products(itemNumbersList)
-    df_final_products = parse_products(data_final_products)
-
-    return df_final_products
+    data_products = get_products(itemNumbersList)
+    df_products = parse_products(data_products)
 
 
 if __name__ == '__main__':
-    # df_final_products = main()
-    # df_final_products.to_csv('output/df_final_products.csv')
+    df_products = main()
     dataJson = clean_data()
-    db_upload(dataJson)
+    #db_upload(dataJson)
